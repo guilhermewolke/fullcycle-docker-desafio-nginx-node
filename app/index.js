cde1775487f83e49f2049d18e289632578520300
Guilherme R. Woelke <guilherme_wolke@yahoo.com>
2023-03-07 15:19:44 -0300

    main
    - atualização do .gitignore para incluir o node_modules na relação de arquivos
    - criação da tabela do banco de dados no index.js
    - alterações no Dockerfile do container do node

--- app/index.js
+++ app/index.js
@@ -11,6 +11,9 @@ const config = {
 
 const mysql = require("mysql");
 const connection = mysql.createConnection(config);
+
+// Criando a tabela ¬¬
+connection.query("CREATE TABLE IF NOT EXISTS `people` (`id` int(11) NOT NULL AUTO_INCREMENT,`name` varchar(100) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=latin1");
 date = new Date();
 var agora = date.toISOString();
 const sql = `INSERT INTO people(name) VALUES ("Pessoa `+ agora + `")`;
